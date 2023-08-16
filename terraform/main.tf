e7dbe42571f5c76417ec0a127336438d5dd9cd3f
aiirononeko <aiirononeko@gmail.com>
2023-08-16 19:38:19 +0900

    Lambda, ApiGatewayのリソース作成

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,134 @@
 ################################
+# IAM Role For Lambda
+################################
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+################################
+# IAM Role For API Gateway
+################################
+
+resource "aws_iam_role" "api_gateway_role" {
+  name               = "apigateway-role"
+  assume_role_policy = data.aws_iam_policy_document.api_gateway_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway_policy_logs" {
+  role       = aws_iam_role.api_gateway_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway_policy_lambda" {
+  role       = aws_iam_role.api_gateway_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaRole"
+}
+
+data "aws_iam_policy_document" "api_gateway_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["apigateway.amazonaws.com"]
+    }
+  }
+}
+
+################################
+# Lambda
+################################
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "../dist"
+  output_path = "dist/build.zip"
+}
+
+resource "aws_lambda_function" "api" {
+  depends_on       = [aws_iam_role.lambda_role]
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = "api"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "index.lambda_handler"
+  runtime          = "nodejs18.x"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
+
+################################
+# API Gateway
+################################
+
+resource "aws_api_gateway_rest_api" "api" {
+  name = "api"
+
+  body = jsonencode({
+    openapi = "3.0.1"
+    info = {
+      title   = "api"
+      version = "1.0"
+    }
+    paths = {
+      "/test" = {
+        get = {
+          x-amazon-apigateway-integration = {
+            httpMethod           = "POST"
+            payloadFormatVersion = "1.0"
+            type                 = "AWS_PROXY"
+            uri                  = aws_lambda_function.api.invoke_arn
+            credentials          = aws_iam_role.api_gateway_role.arn
+          }
+        }
+      }
+    }
+  })
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  depends_on  = [aws_api_gateway_rest_api.api]
+  stage_name  = "prod"
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.api))
+  }
+}
+
+data "aws_iam_policy_document" "api_gateway_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type = "*"
+      identifiers = ["*"]
+    }
+    actions   = ["execute-api:Invoke"]
+    resources = ["${aws_api_gateway_rest_api.api.execution_arn}/*"]
+  }
+}
+
+resource "aws_api_gateway_rest_api_policy" "policy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  policy = data.aws_iam_policy_document.api_gateway_policy.json
+}
+
+################################
 # DynamoDB 
 ################################
 
