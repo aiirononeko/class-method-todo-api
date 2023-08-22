4f0a1934cf41a80b3bd600569f1b9eeb9132e325
aiirononeko <aiirononeko@gmail.com>
2023-08-22 14:13:00 +0900

    TODO取得用Lambdaの作成

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,24 +2,35 @@
 # IAM Role For Lambda
 ################################
 
-resource "aws_iam_role" "lambda_role" {
-  name               = "lambda-role"
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-data "aws_iam_policy_document" "lambda_assume_role" {
+data "aws_iam_policy_document" "assume_role" {
   statement {
-    actions = ["sts:AssumeRole"]
-    effect  = "Allow"
+    effect = "Allow"
+
     principals {
       type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "lambda_dynamodb" {
+  statement {
+    effect = "Allow"
+
+    actions = ["dynamodb:Scan", "dynamodb:GetItem", "dynamodb:PutItem"]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  inline_policy {
+    name   = "policy-8675309"
+    policy = data.aws_iam_policy_document.lambda_dynamodb.json
   }
 }
 
@@ -54,31 +65,31 @@ data "aws_iam_policy_document" "api_gateway_assume_role" {
 }
 
 ################################
-# Lambda
+# Lambda - Retrieve List of TODO
 ################################
 
-data "archive_file" "lambda_zip" {
+data "archive_file" "retrieve_todo_lambda_zip" {
   type        = "zip"
-  source_dir  = "../dist"
-  output_path = "dist/build.zip"
+  source_dir  = "../dist/retrieve"
+  output_path = "dist/retrieve/build.zip"
 }
 
-resource "aws_lambda_function" "api" {
-  depends_on       = [aws_iam_role.lambda_role]
-  filename         = data.archive_file.lambda_zip.output_path
-  function_name    = "api"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "index.lambdaHandler"
+resource "aws_lambda_function" "retrieve-todo" {
+  depends_on       = [aws_iam_role.iam_for_lambda]
+  filename         = data.archive_file.retrieve_todo_lambda_zip.output_path
+  function_name    = "retrieve-todo"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "index.handler"
   runtime          = "nodejs18.x"
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  source_code_hash = data.archive_file.retrieve_todo_lambda_zip.output_base64sha256
 }
 
 ################################
 # API Gateway
 ################################
 
-resource "aws_api_gateway_rest_api" "api" {
-  name = "api"
+resource "aws_api_gateway_rest_api" "todo-api" {
+  name = "todo-api"
 
   body = jsonencode({
     openapi = "3.0.1"
@@ -87,13 +98,13 @@ resource "aws_api_gateway_rest_api" "api" {
       version = "1.0"
     }
     paths = {
-      "/test" = {
+      "/todos" = {
         get = {
           x-amazon-apigateway-integration = {
             httpMethod           = "POST"
             payloadFormatVersion = "1.0"
             type                 = "AWS_PROXY"
-            uri                  = aws_lambda_function.api.invoke_arn
+            uri                  = aws_lambda_function.retrieve-todo.invoke_arn
             credentials          = aws_iam_role.api_gateway_role.arn
           }
         }
@@ -103,11 +114,11 @@ resource "aws_api_gateway_rest_api" "api" {
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  depends_on  = [aws_api_gateway_rest_api.api]
+  rest_api_id = aws_api_gateway_rest_api.todo-api.id
+  depends_on  = [aws_api_gateway_rest_api.todo-api]
   stage_name  = "prod"
   triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.api))
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.todo-api))
   }
 }
 
@@ -119,12 +130,12 @@ data "aws_iam_policy_document" "api_gateway_policy" {
       identifiers = ["*"]
     }
     actions   = ["execute-api:Invoke"]
-    resources = ["${aws_api_gateway_rest_api.api.execution_arn}/*"]
+    resources = ["${aws_api_gateway_rest_api.todo-api.execution_arn}/*"]
   }
 }
 
 resource "aws_api_gateway_rest_api_policy" "policy" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
+  rest_api_id = aws_api_gateway_rest_api.todo-api.id
   policy = data.aws_iam_policy_document.api_gateway_policy.json
 }
 
@@ -133,7 +144,7 @@ resource "aws_api_gateway_rest_api_policy" "policy" {
 ################################
 
 resource "aws_dynamodb_table" "main" {
-  name = "Todos"
+  name = "todos"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
@@ -143,24 +154,4 @@ resource "aws_dynamodb_table" "main" {
     name = "Id"
     type = "S"
   }
-
-  # attribute {
-  #   name = "Title"
-  #   type = "S"
-  # }
-  #
-  # attribute {
-  #   name = "Content"
-  #   type = "S"
-  # }
-  #
-  # attribute {
-  #   name = "Expiration"
-  #   type = "S"
-  # }
-  #
-  # attribute {
-  #   name = "Status"
-  #   type = "S"
-  # }
 }
